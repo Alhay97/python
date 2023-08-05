be46d07f93c66947fdcffdbedb41565d08b82a30
alhai alhameli <Alhay.7@gmail.com>
2023-08-05 18:16:05 +0400

    learn data lists tuples, set, and dictionary

---
+++ dataType.py
@@ -0,0 +1,18 @@
+char = 'a'
+print(char, " :", type(char))
+word = "alhai"
+print(word, " :", type(word))
+num = 12
+print(num , " :", type(num))
+num2 = 12.3
+print(num2 , " :", type(num2))
+binanum = 0b11001
+print(binanum , " :", type(binanum))
+num3 = 21+2j
+print(num3 , " :", type(num3))
+num4 = 10.13123
+print(num4 , " :", type(num4))
+
+
+binanum = 0b11001
+print(binanum , " :", type(binanum))
