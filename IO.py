1c199a5ab0d19574b59c02066262d2c05c599f66
alhai alhameli <Alhay.7@gmail.com>
2023-08-06 20:00:24 +0400

    namespace and flowcontrol

---
+++ IO.py
@@ -0,0 +1,8 @@
+birthdate = int(input('Enter birth date: '))
+
+print('alhai' , 'alhameli', birthdate, sep= " . ")
+
+print('alhai ', 'alhameli', end='\n')
+
+
+
--- dataType.py
+++ dataType.py
@@ -16,3 +16,15 @@ print(num4 , " :", type(num4))
 
 binanum = 0b11001
 print(binanum , " :", type(binanum))
+
+
+# typecasting (explicit type conversion )
+
+num_string  = '121'
+print(type(num_string))
+
+string_num  = int(num_string)
+print(type(string_num))
+
+float_num = float(string_num)
+print(type(float_num))
