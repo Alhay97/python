b9f2ba66f6c9396f49d2c263e0c23e5848e2786f
Alhay97 <alhay.7@gmail.com>
2023-08-08 17:13:30 +0400

    continue for loop

---
+++ forLoop.py
@@ -0,0 +1,11 @@
+# a simple loop that runs through a list
+
+names = ['alhai', 'ahmed', 'alhay', 'alhameli']
+
+for i in names:
+    print(i)
+    
+nm = ['saim' , 'shatha', 'bakoori', 'not me']
+
+for n in nm:
+    print("hello: {}".format(n) )
